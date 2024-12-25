db5afd04015e82c14ec909b799818c1c561c4c24
rbenaim <rbenaim@hotmail.co.uk>
2024-12-25 15:21:54 +0200

    updated

--- models/ClipIdentifier.py
+++ models/ClipIdentifier.py
@@ -4,16 +4,19 @@ from src import HelperFunctions
 from models import CLIPmodel
 
 def multi_image_identifier(input_dir,output_dir,image_list,model_name,device):
-    for id_imgs in os.listdir(input_dir):
+    # for id_imgs in os.listdir(input_dir):
 
-      clip_pipeline = CLIPmodel.CLIPPipeline(device,model_name)
-      df, csv_path = HelperFunctions.get_current_csv(output_dir,id_imgs)
-      for image_name,image,_ in image_list:
-          image_flag = HelperFunctions.row_exist(df,id_imgs,image_name)
-          if image_flag:
-              continue
-          else:
-              images_id_path = os.path.join(input_dir,id_imgs)
-              clip_embeddings = clip_pipeline.images_to_embeddings(images_id_path)
-              loss = clip_pipeline.image_identifier(image,clip_embeddings)
-              HelperFunctions.save_to_csv(loss,df,image_name,csv_path,reverse=True)
+  clip_pipeline = CLIPmodel.CLIPPipeline(device,model_name)
+  for image_name,image,_ in image_list:
+      cls = image_name.rsplit("_", 1)[0]
+
+      df, csv_path = HelperFunctions.get_current_csv(output_dir, cls)
+
+      image_flag = HelperFunctions.row_exist(df,cls,image_name)
+      if image_flag:
+          continue
+      else:
+          # images_path = os.path.join(input_dir,cls)
+          clip_embeddings = clip_pipeline.images_to_embeddings(input_dir)
+          loss = clip_pipeline.image_identifier(image,clip_embeddings)
+          HelperFunctions.save_to_csv(loss,df,image_name,csv_path,reverse=True)
