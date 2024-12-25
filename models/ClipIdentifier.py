204ccccd655efb75dec61683bc36aae47d0ed30e
rbenaim <rbenaim@hotmail.co.uk>
2024-12-25 14:34:13 +0200

    updated

--- models/ClipIdentifier.py
+++ models/ClipIdentifier.py
@@ -4,21 +4,16 @@ from src import HelperFunctions
 from models import CLIPmodel
 
 def multi_image_identifier(input_dir,output_dir,image_list,model_name,device):
-    for image_ID in os.listdir(input_dir):
+    for id_imgs in os.listdir(input_dir):
 
       clip_pipeline = CLIPmodel.CLIPPipeline(device,model_name)
-      if HelperFunctions.image_check(image_ID):
-        cls = image_ID.rsplit(".", 1)[0]
-      else:
-          print("file {} not an image".format(image_ID))
-          break
-      df, csv_path = HelperFunctions.get_current_csv(output_dir,cls)
+      df, csv_path = HelperFunctions.get_current_csv(output_dir,id_imgs)
       for image_name,image,_ in image_list:
-          image_flag = HelperFunctions.row_exist(df,cls,image_name)
+          image_flag = HelperFunctions.row_exist(df,id_imgs,image_name)
           if image_flag:
               continue
           else:
-              image_id_path = os.path.join(input_dir,image_ID)
-              clip_embeddings = clip_pipeline.images_to_embeddings(image_id_path)
+              images_id_path = os.path.join(input_dir,id_imgs)
+              clip_embeddings = clip_pipeline.images_to_embeddings(images_id_path)
               loss = clip_pipeline.image_identifier(image,clip_embeddings)
               HelperFunctions.save_to_csv(loss,df,image_name,csv_path,reverse=True)
