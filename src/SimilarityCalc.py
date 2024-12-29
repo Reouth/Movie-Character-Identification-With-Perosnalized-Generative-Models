6ebfc84b52a769e274ef66a52b4cfccbb958999a
rbenaim <rbenaim@hotmail.co.uk>
2024-12-29 10:57:52 +0200

    updated

--- src/SimilarityCalc.py
+++ src/SimilarityCalc.py
@@ -11,8 +11,7 @@ def calculate_average_precision(input_folder, output_folder, clip_csv=False):
         if csv_file.endswith('.csv'):
             df = pd.read_csv(os.path.join(input_folder, csv_file))
             # Extract the query IDs and add to the set
-            query_ids = df['input_CLIP_embeds'].apply(lambda x: x.rsplit('_', 1)[0]).unique() if clip_csv else df[
-                'input_SD_embeds'].apply(lambda x: x.rsplit('_', 1)[0]).unique()
+            query_ids = df['input_embeds'].apply(lambda x: x.rsplit('_', 1)[0]).unique()
             all_query_ids.update(query_ids)
 
     # Initialize results for all query IDs
@@ -32,12 +31,11 @@ def calculate_average_precision(input_folder, output_folder, clip_csv=False):
             for gt_image, group in grouped_df:
                 # Get the top query (the one with the best score)
                 if clip_csv:
-                    top_query = group.loc[group['CLIP_loss'].idxmax()]  # Use idxmax for CLIP
+                    top_query = group.loc[group['loss'].idxmax()]  # Use idxmax for CLIP
                 else:
-                    top_query = group.loc[group['SD_loss'].idxmin()]  # Use idxmin for SD
+                    top_query = group.loc[group['loss'].idxmin()]  # Use idxmin for SD
 
-                top_query_id = top_query['input_CLIP_embeds'].rsplit('_', 1)[0] if clip_csv else \
-                top_query['input_SD_embeds'].rsplit('_', 1)[0]  # Extract query name without number
+                top_query_id = top_query['input_embeds'].rsplit('_', 1)[0]  # Extract query name without number
 
                 # Check if the top query matches the gallery class
                 if gt_image.startswith(top_query_id):  # Match in the same class
@@ -68,14 +66,9 @@ def calculate_average_precision(input_folder, output_folder, clip_csv=False):
     return ap_scores, map_score
 
 
-def process_value(x):
-    if x.rsplit('_', 1)[-1].isdigit():  # Check if the part after the last underscore is a digit
-        return x.rsplit('_', 1)[0].replace('tinaa', 'Tina')
-    return x
 
 def topk_cls_pred(df, k, correct_class, pred_column, loss, ascend, avg=True):
     df1 = df.groupby("GT Image name")
-    # df1=df.groupby("Image name")
 
     count = 0
     correct = 0
@@ -100,21 +93,21 @@ def topk_cls_pred(df, k, correct_class, pred_column, loss, ascend, avg=True):
     return top_k_percentage
 
 def csv_to_topk_results(avg,clip_csv,k_range,csvs,pred_column,results_folder):
+
     if avg:
         avg_name = 'avg'
     else:
         avg_name = ""
+
+    input_embeds= 'input_embeds'
+    loss = "loss"
     if clip_csv:
         ascend = False
-        input_pred = 'input_CLIP_embeds'
-        loss = 'CLIP_loss'
         model_name = "CLIP_MODEL"
     else:
         ascend = True
-        input_pred = 'input_SD_embeds'
-        # input_pred = 'input_SD'
-        loss = "SD_loss"
         model_name = "SD_MODEL"
+
     for k in k_range:
         print(k)
         top_k_all = 0
@@ -122,15 +115,9 @@ def csv_to_topk_results(avg,clip_csv,k_range,csvs,pred_column,results_folder):
         results = []
         for csv in csvs:
             GT_cls = str(csv).rsplit("/", 1)[1].rsplit("_results", 1)[0]
-            # GT_cls = str(csv).rsplit("/",1)[1].split("_a photo",1)[0].replace(" ","_")
-            print(GT_cls)
             df = pd.read_csv(csv)
-            df[pred_column] = df[input_pred].apply(lambda x: x.rsplit('_', 1)[0]).replace(" ","_")
-            # df[pred_column] = df[input_pred].apply(process_value)
-            # df[pred_column] = df[input_pred].apply(lambda x: x.replace(' ', '_'))
+            df[pred_column] = df[input_embeds].apply(lambda x: x.rsplit('_', 1)[0]).replace(" ","_")
             top_k = topk_cls_pred(df, k, GT_cls, pred_column, loss, ascend, avg)
-            print(top_k)
-            # print("{} predicted for TOP {} accuracy : {}".format(GT_cls, k, top_k))
             results.append({"GT_cls": GT_cls, "Top_k_accuracy": top_k})
             top_k_all += top_k
             count += 1
@@ -141,30 +128,3 @@ def csv_to_topk_results(avg,clip_csv,k_range,csvs,pred_column,results_folder):
         results_path = os.path.join(results_folder, "top_{}_{}_accuracy_results.csv".format(k, avg_name))
         results_df.to_csv(results_path, index=False)
 
-def merge_csv_results(base_path,folders_names,output_dir):
-    # Get the list of csv files from the first folder (assuming all folders have the same csv files)
-    f1_path = os.path.join(base_path, folders_names[0])
-    csv_files = [f for f in os.listdir(f1_path) if f.endswith('.csv')]
-
-    # Dictionary to store dataframes for each csv file
-    csv_data = {csv_file: [] for csv_file in csv_files}
-
-    # Loop through each folder and each csv file
-    for folder in folders_names:
-        for csv_file in csv_files:
-            file_path = os.path.join(base_path, folder, csv_file)
-            df = pd.read_csv(file_path)
-            last_row = df.iloc[-1]
-            last_row['folder_name'] = folder  # Add the folder name as a new column
-            csv_data[csv_file].append(last_row)
-
-    os.makedirs(output_dir, exist_ok=True)
-
-    for csv_file, rows in csv_data.items():
-        merged_df = pd.DataFrame(rows)
-        # Sort the dataframe by 'Top_k_accuracy' column in descending order
-        # sorted_df = merged_df.sort_values(by='Top_k_accuracy', ascending=False)
-        output_path = os.path.join(output_dir, csv_file)
-        merged_df.to_csv(output_path, index=False)
-
-    print("CSV files have been created with the last rows from each folder.")
